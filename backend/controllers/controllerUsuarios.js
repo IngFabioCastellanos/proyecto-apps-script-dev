0eed9252243807b2e0fa3038eed1688f57954f28
fabio castellanos <fcastellanos782@gmail.com>
2022-02-12 17:38:50 -0500

    agregando notificaciones y el primer hook useEffect

--- backend/controllers/controllerUsuarios.js
+++ backend/controllers/controllerUsuarios.js
@@ -5,10 +5,16 @@ function guardarUsuario(usuario) {
     sheetUsuarios.appendRow([id, nombreCompleto, correo, contrasenia]);
     return {
       titulo: "Registro exitoso",
+      descripcion: "Ya se encuentra el usuario en la base de datos.",
     };
   } catch (error) {
     return {
       titulo: "Ops ha ocurrido un error! " + error,
+      descripcion: "Por favor contacte a soporte.",
     };
   }
+}
+
+function listarUsuarios() {
+  return obtenerDatos(env_().SH_REGISTRO_USUARIOS);
 }
